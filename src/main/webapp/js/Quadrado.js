2b345f951f166da032f27cad5ce02bf51a312bd8
jonasleal <mufireoflife@hotmail.com>
2017-03-31 19:30:54 -0300

    Correcao erro do quadrado

--- src/main/webapp/js/Quadrado.js
+++ src/main/webapp/js/Quadrado.js
@@ -3,31 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var Quadrado = function(x , y){
+Quadrado = function (x, y){
     var centroX = x;
     var centroY = y;
     var tamanho = 30;
-    
-    var div1 = document.createElement("div");
-    div1.setAttribute("style","width:"+tamanho+"px;height:"+tamanho
-            +"px;background:yellow;top:"+(centroY)+"px;left:"+(centroX)
-            +"px;position:absolute");
-    var div2 = document.createElement("div");
-    div2.setAttribute("style","width:"+tamanho+"px;height:"+tamanho
-            +"px;background:yellow;top:"+(centroY+tamanho)+"px;left:"+(centroX)
-            +"px;position:absolute");
-    var div3 = document.createElement("div");
-    div3.setAttribute("style","width:"+tamanho+"px;height:"+tamanho
-            +"px;background:yellow;top:"+(centroY)+"px;left:"+(centroX+tamanho)
-            +"px;position:absolute");
-    var div4 = document.createElement("div");
-    div4.setAttribute("style","width:"+tamanho+"px;height:"+tamanho
-            +"px;background:yellow;top:"+(centroY+tamanho)+"px;left:"+(centroX+tamanho)
-            +"px;position:absolute");
-    };
-    Quadrado.prototype.mostrar = function () {
-        var div = document.getElementById("divPrincipal");
-        div.appendChild(div1);
-    };
 
+    var div1 = document.createElement("div");
+    div1.setAttribute("style", "width:" + tamanho + "px;height:" + tamanho
+            + "px;background:yellow;top:" + (centroY) + "px;left:" + (centroX)
+            + "px;position:absolute;border: solid 1px;");
+    var div2 = document.createElement("div");
+    div2.setAttribute("style", "width:" + tamanho + "px;height:" + tamanho
+            + "px;background:yellow;top:" + (centroY + tamanho) + "px;left:" + (centroX)
+            + "px;position:absolute;border: solid 1px;");
+    var div3 = document.createElement("div");
+    div3.setAttribute("style", "width:" + tamanho + "px;height:" + tamanho
+            + "px;background:yellow;top:" + (centroY) + "px;left:" + (centroX + tamanho)
+            + "px;position:absolute;border: solid 1px;");
+    var div4 = document.createElement("div");
+    div4.setAttribute("style", "width:" + tamanho + "px;height:" + tamanho
+            + "px;background:yellow;top:" + (centroY + tamanho) + "px;left:" + (centroX + tamanho)
+            + "px;position:absolute;border: solid 1px;");
+
+    
+
+this.mostrar=function () {
+    var div = document.getElementById("divPrincipal");
+    div.appendChild(div1);
+    div.appendChild(div2);
+    div.appendChild(div3);
+    div.appendChild(div4); 
+};
+};
 
