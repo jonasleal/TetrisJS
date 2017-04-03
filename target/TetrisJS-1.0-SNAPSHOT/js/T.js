3de8d893566318e4216e6c4d0da6c8bf9c99e356
Samuel Soares <samuelsoares179@gmail.com>
2017-04-03 10:13:00 -0300

    Adicionando movimentos para peças I e T

--- target/TetrisJS-1.0-SNAPSHOT/js/T.js
+++ target/TetrisJS-1.0-SNAPSHOT/js/T.js
@@ -3,42 +3,100 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-        
 
-T = function(x,y){
+
+T = function (x, y) {
     var centroX = x;
     var centroY = y;
     var tamanho = 30;
-    var cor ="purple";
-    
+    var cor = "purple";
+
     //Div 1 será a peça do meio e a peça de movimento
     var div1 = document.createElement("div");
-    div1.setAttribute("style","width:"+(tamanho-1)+"px;height:"+(tamanho-1)+"px;background:"+cor+";top:"+
-            (centroY)+"px;left:"+(centroX)+"px;position:absolute;border: solid 1px");
-    
+    div1.setAttribute("style", "width:" + (tamanho - 1) + "px;height:" + (tamanho - 1) + "px;background:" + cor + ";top:" +
+            (centroY) + "px;left:" + (centroX) + "px;position:absolute;border: solid 1px");
+
     var div2 = document.createElement("div");
-    div2.setAttribute("style","width:"+(tamanho-1)+"px;height:"+(tamanho-1)+"px;background:"+cor+";top:"+
-            (centroY+tamanho)+"px;left:"+(centroX)+"px;position:absolute;border: solid 1px");
-    
+    div2.setAttribute("style", "width:" + (tamanho - 1) + "px;height:" + (tamanho - 1) + "px;background:" + cor + ";top:" +
+            (centroY + tamanho) + "px;left:" + (centroX) + "px;position:absolute;border: solid 1px");
+
     var div3 = document.createElement("div");
-    
-    div3.setAttribute("style","width:"+(tamanho-1)+"px;height:"+(tamanho-1)+"px;background:"+cor+";top:"+
-            (centroY)+"px;left:"+(centroX-tamanho)+"px;position:absolute;border: solid 1px");
-    
-    var div4= document.createElement("div");
-    
-    div4.setAttribute("style", "width:"+(tamanho-1)+"px;height:"+(tamanho-1)+"px;background:"+cor+";top:"+
-            (centroY)+"px;left:"+(centroX+tamanho)+"px;position:absolute;border: solid 1px");
-    
-    
-    this.mostrar=function(){
+
+    div3.setAttribute("style", "width:" + (tamanho - 1) + "px;height:" + (tamanho - 1) + "px;background:" + cor + ";top:" +
+            (centroY) + "px;left:" + (centroX - tamanho) + "px;position:absolute;border: solid 1px");
+
+    var div4 = document.createElement("div");
+
+    div4.setAttribute("style", "width:" + (tamanho - 1) + "px;height:" + (tamanho - 1) + "px;background:" + cor + ";top:" +
+            (centroY) + "px;left:" + (centroX + tamanho) + "px;position:absolute;border: solid 1px");
+
+
+    this.mostrar = function () {
         var div = document.getElementById("divPrincipal");
         div.appendChild(div1);
         div.appendChild(div2);
         div.appendChild(div3);
         div.appendChild(div4);
     };
-    
+
+    this.moverEsquerda = function () {
+        centroX = centroX - tamanho;
+        div1.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background:" + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX) + "px; position: absolute;border: solid 1px");
+
+        div2.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background:" + cor + ";top:" +
+                (centroY + tamanho) + "px; left:" + (centroX) + "px; position: absolute;border: solid 1px");
+
+        div3.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background:" + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX - tamanho) + "px; position: absolute;border: solid 1px");
+
+        div4.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background:" + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX + tamanho) + "px; position: absolute;border: solid 1px");
+
+    };
+
+
+    this.moverDireita = function () {
+        centroX = centroX + tamanho;
+
+        div1.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX) + "px; position: absolute; border: solid 1px");
+
+        div2.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY + tamanho) + "px; left:" + (centroX) + "px; position: absolute; border: solid 1px");
+
+        div3.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX - tamanho) + "px; position: absolute; border: solid 1px");
+
+
+        div4.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX + tamanho) + "px; position: absolute; border: solid 1px");
+
+
+    };
+
+
+    this.moverBaixo = function () {
+        centroY = centroY + tamanho;
+
+
+        div1.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX) + "px; position: absolute; border: solid 1px");
+
+        div2.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY + tamanho) + "px; left:" + (centroX) + "px; position: absolute; border: solid 1px");
+
+        div3.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX - tamanho) + "px; position: absolute; border: solid 1px");
+
+
+        div4.setAttribute("style", "width:" + (tamanho - 1) + "px; height:" + (tamanho - 1) + "px; background: " + cor + ";top:" +
+                (centroY) + "px; left:" + (centroX + tamanho) + "px; position: absolute; border: solid 1px");
+
+    };
+
+
+
 };
 
 
