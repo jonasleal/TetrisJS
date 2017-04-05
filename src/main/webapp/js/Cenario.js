7e4e722a4e6883541909d85a9c6e61d555ff70b3
jonasleal <mufireoflife@hotmail.com>
2017-04-05 15:37:24 -0300

    Adicao de movimento de queda

--- src/main/webapp/js/Cenario.js
+++ src/main/webapp/js/Cenario.js
@@ -5,8 +5,8 @@
  */
 
 Cenario = function () {
-    var largura = 300;
-    var altura = 500;
+    var largura = 341;
+    var altura = 510;
     var proximo;
     var div1 = document.createElement("div");
     var div2 = document.createElement("div");
@@ -15,47 +15,89 @@ Cenario = function () {
     var divStatus = document.createElement("div");
 
 
-    divPrincipal.setAttribute("id", "divPrincipal");
-    divPrincipal.setAttribute("style", "background:gainsboro; width:" + largura + "px; height:" + altura + "px; border: solid brown 30px; float: left;");
-    divPreview.setAttribute("id", "divPreview");
-    divPreview.setAttribute("style", "background:gainsboro; margin-top : 30px; width:" + 150 + "px; height:" + 150 + "px; border: solid black 2px;");
-    divStatus.setAttribute("id", "divStatus");
-    divStatus.setAttribute("style", "background:gainsboro; width:" + 150 + "px; height:" + 350 + "px; top : 500px; left : 500px border: solid black 2px;");
-    div2.setAttribute("style", "float: left");
-    //  teste
-    
-    var tabela = document.createElement("table");
-    tabela.setAttribute("id", "tabelaPrincipal");
-    tabela.setAttribute("style", "border-collapse: collapse;border: -1px solid black;");
-    for( i = 0; i < 15 ; i++){
-         linha = document.createElement("tr");
-         //linha.setAttribute("style", "border: 1px solid black;");
-         for (j = 0 ; j < 10 ; j++){
-             coluna = document.createElement("td");
-             coluna.setAttribute("style","width: 30px;height: 30px;border: 1px solid black;");
-             linha.appendChild(coluna);
-         }
-         tabela.appendChild(linha);
-        
-    }
-    divPrincipal.appendChild(tabela);
+    iniciarJogo = function () {
+        proximo = (sorteio());
+        setInterval(interacao, 1000);
+    };
 
-    //  teste
-    div1.appendChild(divPrincipal);
-    div2.appendChild(divPreview);
-    div2.appendChild(divStatus);
-    div1.appendChild(div2);
-    document.body.appendChild(div1);
-
-
+    sorteio = function () {
+        sorteado  = Math.floor(Math.random() * 4);
+        console.log(sorteado);
+        var tetraminos = [new Quadrado(4, 0), new I(4, 0)];
+        return tetraminos[0];
+    };
 
     this.preview = function (_tetramino) {
         proximo = _tetramino;
         proximo.posiciona(415, 80);
         proximo.mostrar();
     };
-    this.proximaJogada = function () {
-        proximo.posiciona(160, -30);
-        return proximo;
+
+    proximaJogada = function () {
+        tetramino = proximo;
+        tetramino.posiciona(4, 0);
+        tetramino.mostrar();
+        proximo = (sorteio());
+        
     };
+
+    interacao = function () {
+        if (!tetramino.moverBaixo()) {
+            proximaJogada();
+        }
+    };
+    this.criaCentario = function () {
+        divPrincipal.setAttribute("id", "divPrincipal");
+        divPrincipal.setAttribute("style", "background:gainsboro; width:" + largura + "px; height:" + altura + "px; border: solid brown 30px; float: left;");
+        divPreview.setAttribute("id", "divPreview");
+        divPreview.setAttribute("style", "background:gainsboro; margin-top : 30px; width:" + 150 + "px; height:" + 125 + "px; border: solid black 2px;");
+        divStatus.setAttribute("id", "divStatus");
+        divStatus.setAttribute("style", "background:gainsboro; width:" + 150 + "px; height:" + 350 + "px; top : 500px; left : 500px border: solid black 2px;");
+        div2.setAttribute("style", "float: left");
+
+        var tabelaPrincipal = document.createElement("table");
+        tabelaPrincipal.setAttribute("id", "tabelaPrincipal");
+        tabelaPrincipal.setAttribute("style", "border-collapse: collapse;border: 1px solid black;");
+        for (i = 0; i < 15; i++) {
+            linha = document.createElement("tr");
+            //linha.setAttribute("style", "border: 1px solid black;");
+            for (j = 0; j < 10; j++) {
+                coluna = document.createElement("td");
+                coluna.setAttribute("style", "width: 31px;height: 31px;border: 1px solid black;");
+                linha.appendChild(coluna);
+            }
+            tabelaPrincipal.appendChild(linha);
+
+        }
+
+        var tabelaPreview = document.createElement("tabela");
+        tabelaPreview.setAttribute("id", "tabelaPreview");
+        tabelaPreview.setAttribute("style", "border-collapse: collapse;border: 1px solid black;");
+        for (i = 0; i < 4; i++) {
+            linha = document.createElement("tr");
+            for (j = 0; j < 4; j++) {
+                coluna = document.createElement("td");
+                coluna.setAttribute("style", "width: 31px;height: 31px;border: 1px solid black;");
+                linha.appendChild(coluna);
+            }
+            tabelaPreview.appendChild(linha);
+
+        }
+
+
+        divPrincipal.appendChild(tabelaPrincipal);
+        // divPreview.appendChild(tabelaPreview);
+        //divPreview.appendChild(tabelaPreview);
+
+        div1.appendChild(divPrincipal);
+        div2.appendChild(divPreview);
+        div2.appendChild(divStatus);
+        div1.appendChild(div2);
+        document.body.appendChild(div1);
+
+        iniciarJogo();
+    };
+
 };
+
+
